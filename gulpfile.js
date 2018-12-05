b4f3b33f5a7a0481d6db38486a97507b0ab91a18
Alexander Zverev <azverev@lulusoftware.com>
2018-12-05 10:52:12 -0500

    s

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,21 @@ var minifyCSS = require('gulp-csso');
 var concat = require('gulp-concat');
 //var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('html', function () {
+
+
+gulp.task('html:index', () => {
 
   return gulp.src('src/*.html')
     .pipe(gulp.dest('dist'))
 });
 
-gulp.task('html:views', function () {
+gulp.task('html:views', () => {
 
   return gulp.src('src/views/**/*.html')
     .pipe(gulp.dest('dist/views'))
 });
+
+gulp.task('html', gulp.series(gulp.parallel('html:index', 'html:views')));
 
 
 gulp.task('css:bootstrap', function () {
@@ -35,29 +39,29 @@ gulp.task('css:admin-lte', function () {
     .pipe(gulp.dest('dist/css'))
 });
 
+gulp.task('css', gulp.series(gulp.parallel('css:bootstrap', 'css:admin-lte')));
 
-gulp.task('js', function () {
+
+gulp.task('js:app', function () {
 
   return gulp.src('src/js/**/*.js')
     .pipe(concat('app.js'))
     .pipe(gulp.dest('dist/js'))
 });
-gulp.task('js:vendor', function () {
+gulp.task('js:angular', function () {
 
-  return gulp.src(
-    ['node_modules/angular/angular.min.js',
-      'node_modules/angular-route/angular-route.min.js',
-      'node_modules/angular-websocket/dist/angular-websocket.min.js'
-    ])
-    .pipe(concat('vendor.js'))
+  return gulp.src([
+    'node_modules/angular/angular.min.js',
+    'node_modules/angular-route/angular-route.min.js',
+    'node_modules/angular-websocket/dist/angular-websocket.min.js'
+  ])
+    .pipe(concat('angular.min.js'))
     .pipe(gulp.dest('dist/js'))
 });
 
-gulp.task('default', gulp.series(gulp.parallel(
-  'html', 'html:views',
-  'css:bootstrap','css:admin-lte',
-  'js', 'js:vendor'
-)));
+gulp.task('js', gulp.series(gulp.parallel('js:app', 'js:angular')));
+
+gulp.task('default', gulp.series(gulp.parallel('html', 'css', 'js')));
 
 
 
