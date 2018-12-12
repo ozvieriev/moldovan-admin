3fe092ff446e3009b3a8276642782caddd2ee4c0
Alexander Zverev <azverev@lulusoftware.com>
2018-12-12 13:44:51 -0500

    ui

--- gulpfile.js
+++ gulpfile.js
@@ -91,7 +91,7 @@ gulp.task('font:bootstrap', () => {
   return gulp.src([
     'node_modules/bootstrap/dist/fonts/glyphicons-halflings-regular.woff2'
   ])
-    .pipe(gulp.dest('dist/css/fonts'))
+    .pipe(gulp.dest('dist/fonts'))
 });
 gulp.task('font', gulp.series(gulp.parallel('font:bootstrap')));
 
